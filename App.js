cdab76cb8fa3f038db9a1465613a2ae03c5c1bb7
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-09-10 20:50:20 -0300

    feat(App): Configuração do APP

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+import Header from "./src/components/Header";
+import Router from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "#F6E48A",
+      }}
+    >
+      <Header />
+      <Router />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
