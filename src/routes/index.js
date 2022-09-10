6e31bb356021b8fa502d99aef37ff56c400865d0
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-09-10 20:48:57 -0300

    feat(Routes): Implementado o component Router

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "../screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
