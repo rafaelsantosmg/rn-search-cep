9b04d22cef9bea10b8a25b5436d820bf9dad6627
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-09-11 00:31:02 -0300

    feat(Home): Adicionado o component card na home e implementado a logica para renderizar e passar suas devidas informações!

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import Card from "../../components/Card";
+import { getCep } from "../../services/index";
 
 const Home = () => {
   const [cep, setCep] = useState("");
+  const [activeScreen, setActiveScreen] = useState("primary");
+  const [listCeps, setListCeps] = useState([]);
+  const [isRender, setIsRender] = useState(false);
 
   const maskCep = (cep) => {
     cep = cep.replace(/\D/g, "");
@@ -10,16 +23,33 @@ const Home = () => {
     setCep(cep);
   };
 
+  const handlePress = async () => {
+    const response = await getCep(cep.replace(/\D/g, ""));
+    if (response.erro) {
+      Alert.alert("Cep Inválido");
+    } else {
+      listCeps.push(response);
+    }
+    setCep("");
+    setIsRender(!isRender);
+  };
+
+  useEffect(() => {
+    setListCeps(listCeps);
+  }, [isRender]);
+
   return (
-    <View style={styles.container}>
-      <View style={{ margin: 20, marginTop: 40 }}>
-        <Text style={styles.text}>Bem vindo a ViaCep</Text>
-        <Text style={styles.text}> </Text>
+    <ScrollView style={styles.container}>
+      <View style={{ marginLeft: 20, marginRight: 20, marginTop: 20 }}>
+        <Text style={{ ...styles.text, textAlign: "center" }}>
+          BEM VINDO AO APP VIACEP
+        </Text>
         <Text style={styles.text}>
-          Aqui você pode fazer sua busca de endereços por cep de todo o Brasil!
+          Aqui você pode fazer sua busca de endereços por "CEP" de todo o
+          Brasil!
         </Text>
       </View>
-      <View style={{ margin: 20, marginTop: 40 }}>
+      <View style={{ margin: 20, marginBottom: 40 }}>
         <Text style={styles.text}>CEP:</Text>
         <TextInput
           maxLength={9}
@@ -28,15 +58,63 @@ const Home = () => {
           style={styles.input}
           value={cep}
         />
-        <Button color="#478203" title="Buscar" />
+        <TouchableOpacity
+          style={styles.buttonAdd}
+          onPress={handlePress}
+          disabled={cep.length !== 9}
+        >
+          <Text style={styles.textAdd}>ADICIONAR</Text>
+        </TouchableOpacity>
       </View>
-    </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={
+            activeScreen === "primary"
+              ? styles.activeTouchable
+              : styles.unactiveTouchable
+          }
+          onPress={() => setActiveScreen("primary")}
+        >
+          <Text
+            style={
+              activeScreen === "primary"
+                ? styles.activeTouchableText
+                : styles.unactiveTouchableText
+            }
+          >
+            UF - Cidade
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={
+            activeScreen === "secundary"
+              ? { ...styles.activeTouchable }
+              : { ...styles.unactiveTouchable }
+          }
+          onPress={() => setActiveScreen("secundary")}
+        >
+          <Text
+            style={
+              activeScreen === "secundary"
+                ? { ...styles.activeTouchableText }
+                : { ...styles.unactiveTouchableText }
+            }
+          >
+            Cidade - Logradouro
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {listCeps.length !== 0 && activeScreen === "primary" ? (
+        <Card ceps={listCeps} primary />
+      ) : (
+        <Card ceps={listCeps} />
+      )}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     backgroundColor: "#F6E48A",
     display: "flex",
     flex: 1,
@@ -45,9 +123,8 @@ const styles = StyleSheet.create({
   text: {
     color: "#478203",
     marginBottom: 5,
-    fontSize: 25,
+    fontSize: 20,
     fontWeight: "bold",
-    lineHeight: 30,
     textAlign: "justify",
   },
   input: {
@@ -60,10 +137,49 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     paddingLeft: 10,
     textAlign: "justify",
-    width: 300,
+    width: "100%",
   },
-  button: {
+  buttonAdd: {
     backgroundColor: "#478203",
+    height: 40,
+    borderRadius: 5,
+    width: "100%",
+  },
+  textAdd: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: "center",
+    padding: 6,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    backgroundColor: "#478203",
+    justifyContent: "space-around",
+    width: "100%",
+    paddingTop: 5,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  activeTouchable: {
+    borderBottomWidth: 3,
+    borderBottomColor: "#fff",
+    color: "#fff",
+    paddingBottom: 10,
+    width: "50%",
+    alignItems: "center",
+  },
+  unactiveTouchable: {
+    width: "50%",
+    alignItems: "center",
+  },
+  activeTouchableText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  unactiveTouchableText: {
+    fontSize: 15,
     color: "#fff",
   },
 });
